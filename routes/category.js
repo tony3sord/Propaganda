a5f4395ecd904c078be74068115f602972c37ab7
tony3sord <133953601+tony3sord@users.noreply.github.com>
2024-02-28 15:55:59 -0500

    Buenas Practicas

--- routes/category.js
+++ routes/category.js
@@ -8,50 +8,6 @@ router.get("/allcategory", async (req, res) => {
 	res.json({ category });
 });
 
-//show the viwe of all category
-router.get("/category", async (req, res) => {
-	const category = await Category.find();
-	// if(req.isAuthenticated()){
-	//     if(req.user.role=="admin"){
-	//         res.render('category',{category});
-	//     }else{
-	//         res.redirect('/');
-	//     }
-	// }else{
-	//     res.redirect("/login");
-	// }
-	res.render("category", { category });
-});
-
-//show the view edit a category with the category to edit
-router.get("/editcategory/:id", async (req, res) => {
-	const category = await Category.findById(req.params.id);
-	// if(req.isAuthenticated()){
-	//     if(req.user.role=="admin"){
-	//         res.render('editcategory', {category});
-	//     }else{
-	//         res.redirect('/');
-	//     }
-	// }else{
-	//     res.redirect('/login')
-	// }
-	res.render("editcategory", { category });
-});
-
-//show the view add a category
-router.get("/addcategory", (req, res) => {
-	// if(req.isAuthenticated()){
-	//     if(req.user.role=="admin"){
-	//         res.render('addcategory');
-	//     }else{
-	//         res.redirect('/');
-	//     }
-	// }else{
-	//     res.redirect('/login');
-	// }
-	res.render("addcategory");
-});
-
 //delete category
 router.delete("/removecategory/:id", async (req, res) => {
 	const id = req.params.id;
