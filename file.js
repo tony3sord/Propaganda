62500ecd80f6960fc62d58f126865c9e61337de5
tony3sord <133953601+tony3sord@users.noreply.github.com>
2024-03-02 12:16:44 -0500

    Minio

--- file.js
+++ file.js
@@ -3,7 +3,7 @@ const { Client } = await import("minio");
 const accessKey = process.env.MINIO_ACCESS_KEY;
 const secretKey = process.env.MINIO_SECRET_KEY;
 const endPoint = process.env.MINIO_END_POINT;
-const port = process.env.MINIO_PORT;
+const port = parseInt(process.env.MINIO_PORT);
 
 const minioClient = new Client({
 	endPoint: endPoint,
